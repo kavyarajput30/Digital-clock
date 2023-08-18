f50988c5c87a025497a4c1e04945128a9193b51f
kavya rajput <131941640+rajputrajput30@users.noreply.github.com>
2023-08-18 18:50:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let clock = document.getElementById("clock");
+
+
+setInterval(()=>{
+    let date = new Date();
+    let time =date.toLocaleTimeString();
+    clock.innerHTML= time;
+    
+},1000) 
